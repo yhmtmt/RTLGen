f251105bedd542800001e68a205e6fb0910163cb
Yohei Matsumoto <yhmtmt@gmail.com>
2026-05-17 19:22:19 +0900

    npu: add quality-backed kv8 hbm frontier (#621)

--- control_plane/control_plane/artifact_policy.py
+++ control_plane/control_plane/artifact_policy.py
@@ -30,6 +30,7 @@ _ALLOWED_DATASET_PREFIXES = {
     "decoder_attention_kv_spill_scheduler__",
     "decoder_attention_kv_hbm_controller__",
     "decoder_attention_kv_physical_hbm_frontier__",
+    "decoder_attention_kv_physical_hbm_quality_backed__",
     "decoder_attention_kv_quality_gate__",
     "decoder_attention_kv_quality_proxy__",
     "decoder_attention_kv_native_gqa_proxy__",
